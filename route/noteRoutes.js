cf0af070862dd414157415deb6d002b96a9a41e2
Samuel Christopher <Ebusameric@gmail.com>
2019-08-22 20:51:55 +0100

    features: add the update note route

--- route/noteRoutes.js
+++ route/noteRoutes.js
@@ -44,14 +44,28 @@ app.route('/create')
 	});
 
 app.route('/:id')
+
 	.get(token_validation, (req, res) => {
 		const { id } = req.params;
 		Note.findById(id)
-			.then(user => {
-				res.json({user});
+			.then(note => res.json({note}) )
+			.catch(e => res.status(404).json({message: 'Note not found!'}));
+	})
+
+	.put(token_validation, (req, res) => {
+		const { id } = req.params;
+		const { title, body, topic } = req.body;
+		Note.findById(id)
+			.then(note => {
+				note.title = title;
+				note.body = body;
+				note.topic = topic;
+				note.save()
+					.then(saveNote => res.json({ success: true, saveNote }))
+					.catch(e => res.status(500).json(e));
 			})
-			.catch(e => res.status(404).json(e));
-	});
+			.catch(e => res.status(404).json({message: 'Note not found!'}));
+	})
 
 
 
