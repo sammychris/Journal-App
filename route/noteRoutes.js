971a7283d8d83dc04079dab92eb5df6e465aed58
Samuel Christopher <Ebusameric@gmail.com>
2019-08-22 20:22:24 +0100

    make some changes

--- route/noteRoutes.js
+++ route/noteRoutes.js
@@ -1,50 +1,46 @@
 import { Router } from 'express';
-import { Note, NoteTopic } from '../models';
+import { Note } from '../models';
 
 const app = Router();
 
 
 const token_validation = (req, res, next) => {
 	// Nothing for now...
-	console.log('The main cause of life');
+	for ( let key in req.body ) {
+		req.body[key] = req.body[key].trim().toLowerCase();
+		if (!req.body[key]) return res.json({ message: 'No empty field!'});
+	}
 	next();
 }
 
 
-app.route('/newtopic')
-	.post(token_validation, (req, res) => {
-		for ( let key in req.body ) {
-			if (!req.body[key].trim()) return res.json({ message: 'This is so wrong!'});
-		}
-	
-		new NoteTopic({ topic: req.body.topic })
+// app.route('/newtopic')
+// 	.post(token_validation, (req, res) => {		
+// 		const { topic } = req.body;
+
+// 		NoteTopic.findOne({ topic })
+// 			.then((foundTopic) => {
+// 				if (foundTopic) return res.json({ message: 'Topic already exists' });
+// 				new NoteTopic({ topic })
+// 					.save()
+// 					.then((note) => {
+// 						return res.json({ message: 'You note has been published!', note })
+// 					})
+// 					.catch(e => res.json(e));
+// 			})
+// 	});
+
+
+app.route('/create')
+	.post(token_validation, (req, res) => {		
+		const { author, title, body, topic } = req.body;
+
+		new Note({ author, title, body, topic })
 			.save()
 			.then((note) => {
 				return res.json({ message: 'You note has been published!', note })
 			})
 			.catch(e => res.json(e));
-	});
-
-
-app.route('/create')
-	.post(token_validation, (req, res) => {
-
-		for ( let key in req.body ) {
-			if (!req.body[key].trim()) return res.json({ message: 'This is so wrong!'});
-		}
-		
-		const { title, body, author, topic, topic_id } = req.body;
-
-		NoteTopic.findById(topic_id)
-			.then((noteTopic) => {
-				new Note({ title, body, author, topic })
-					.save()
-					.then((note) => {
-						NoteTopic.notes.push(note);
-						return res.json({ message: 'You note has been published!', note })
-					})
-					.catch(e => res.json(e));
-			})
 	});
 
 
