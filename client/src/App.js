2abb73516ba4e4f3c039720decc7edf4d035a1ac
Samuel Christopher <Ebusameric@gmail.com>
2019-08-26 16:12:01 +0100

    features: add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
+import { LoginPage } from './LoginPage';
+import { HomePage } from './HomePage';
 import './App.scss';
 
+const ProfilePage = <div></div>;
+const AuthorPage = <div></div>;
 class App extends React.Component {
   render (){
     return (
-      <div>
-        this is so cool.
-      </div>
+      <BrowserRouter>
+        <Route exact path="/" component={LoginPage} />
+        <Route path="/home" component={HomePage} />
+        <Route path="/profile" component={ProfilePage} />
+        <Route path="/author" component={AuthorPage} />
+      </BrowserRouter>
     )
   }
 }
